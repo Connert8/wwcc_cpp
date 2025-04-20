ff41f383b2fe46a3d21e2e88864eaccdc5840e94
Connert8 <connertorrescano8@gmail.com>
2025-04-19 20:06:15 -0700

    Fixes if else statements and finishes program -- CT

--- week2/assignment1-coinflip.cpp
+++ week2/assignment1-coinflip.cpp
@@ -7,9 +7,9 @@ int main() {
     // Seed the random number generator
     srand(time(0));
 
-    int flips = 0;
-    int heads = 0;
-    int tails = 0;
+    double flips = 0;
+    double heads = 0;
+    double tails = 0;
 
     cout << "Coin Flip Simulator" << endl;
     cout << "How many flips would you like to simulate? ";
@@ -19,10 +19,10 @@ int main() {
     for (int i = 0; i < flips; i++) {
         int rando = rand() % 2;
         cout << rando << endl;
-        if (rando = 1) {
+        if (rando == 1) {
             heads += 1;
         }
-        else if (rando = 0) {
+        else if (rando == 0) {
             tails += 1;
         }
         else {
@@ -30,12 +30,12 @@ int main() {
         }
     }
 
-    float htot = heads/flips;
-    float ttot = tails/flips;
+    double htot = heads/flips;
+    double ttot = tails/flips;
 
     cout << "Results: " << "\n";
-    printf("%-5s | %5d | %5d\n", "Heads:", heads, htot);
-    printf("%-5s | %5d | %5d\n", "Tails:", tails, ttot);
+    printf("%-5s | %5.0f | %5.2f\n", "Heads:", heads, htot);
+    printf("%-5s | %5.0f | %5.2f\n", "Tails:", tails, ttot);
 
 
     return 0;
